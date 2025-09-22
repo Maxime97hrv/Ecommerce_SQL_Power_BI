7810c813f376578aa265759ada8b599456f8d259
Maxime <maxime.herve97@gmail.com>
2025-09-22 13:37:24 +0200

    creation of datasets with sql

--- Ecomm.sql
+++ Ecomm.sql
@@ -1,6 +1,9 @@
 ---------------------------------------------------- IMPORT OF THE DATA BASE -----------------------------------------------------------
 
 -- List of Orders.csv
+DROP SCHEMA IF EXISTS Public CASCADE;
+DROP SCHEMA IF EXISTS clean_ecomm CASCADE;
+
 CREATE SCHEMA Public; -- for the data imported
 CREATE SCHEMA clean_ecomm; -- Create the folder schema Clean_ecomm to put our datasets
 
@@ -137,6 +140,7 @@ select
 sum(amount) as total_amount,
 sum(profit) as total_profit,
 sum(quantity) as total_quantity,
+100*sum(profit)/sum(amount) as marge,
 count(distinct order_id) as number_of_orders
 from clean_ecomm.order_details;
 
@@ -150,6 +154,7 @@ select
 order_id,
 sum(amount) as amount_per_order,
 sum(profit) as profit_per_order,
+100*sum(profit)/sum(amount) as marge,
 sum(quantity) as quantity_per_order
 from clean_ecomm.order_details
 group by order_id
@@ -160,7 +165,9 @@ select
 category,
 sum(amount) as amount_per_category,
 sum(profit) as profit_per_category,
-sum(quantity) as quantity_per_category
+100*sum(profit)/sum(amount) as marge,
+sum(quantity) as quantity_per_category,
+100*sum(amount)/sum(sum(amount)) over() as market_share  -- Market share
 from clean_ecomm.order_details
 group by category
 order by profit_per_category asc;
@@ -171,7 +178,9 @@ category,
 sub_category,
 sum(amount) as amount_per_sub_category,
 sum(profit) as profit_per_sub_category,
-sum(quantity) as quantity_per_sub_category
+100*sum(profit)/sum(amount) as marge,
+sum(quantity) as quantity_per_sub_category,
+100*sum(amount)/sum(sum(amount)) over() as market_share  -- Market share
 from clean_ecomm.order_details
 group by category,sub_category 
 order by category, profit_per_sub_category desc;
@@ -181,6 +190,7 @@ select
 l.customername,
 sum(o.amount) as amount_per_customer,
 sum(o.profit) as profit_per_customer,
+100*sum(profit)/sum(amount) as marge,
 sum(o.quantity) as quantity_per_customer
 from clean_ecomm.list_of_orders l
 inner join clean_ecomm.order_details o on o.order_id = l.order_id 
@@ -192,6 +202,7 @@ select
 l.city,
 sum(o.amount) as amount_per_city,
 sum(o.profit) as profit_per_city,
+sum(profit)/sum(amount) as marge,
 sum(o.quantity) as quantity_per_city
 from clean_ecomm.list_of_orders l
 inner join clean_ecomm.order_details o on o.order_id = l.order_id 
@@ -203,6 +214,7 @@ select
 l.state,
 sum(o.amount) as amount_per_customer,
 sum(o.profit) as profit_per_customer,
+sum(profit)/sum(amount) as marge,
 sum(o.quantity) as quantity_per_customer
 from clean_ecomm.list_of_orders l
 inner join clean_ecomm.order_details o on o.order_id = l.order_id 
@@ -216,6 +228,7 @@ date_trunc('month', l.order_date) as order_month, -- Type date with month as uni
 TO_CHAR(l.order_date, 'Mon-YY') AS order_date_label, --display format MMM-YY ex Apr-18 (month of the year)
 sum(o.amount) as amount, -- we want the revenue total by month, same for the profit and quantity
 sum(o.profit) as profit,
+sum(profit)/sum(amount) as marge,
 sum(o.quantity) as quantity
 from clean_ecomm.list_of_orders l
 inner join clean_ecomm.order_details o on o.order_id = l.order_id
@@ -223,12 +236,15 @@ group by date_trunc('month', l.order_date), to_char(l.order_date,'Mon-YY')) -- d
 
 select
 obm.order_date_label,
-obm.amount, obm.profit, obm.quantity,
+obm.amount, 
+lag(obm.amount) over(order by obm.order_month) as previsous_month_amount,
+(obm.amount - lag(obm.amount) over(order by obm.order_month))/(lag(obm.amount) over(order by obm.order_month)) as growth_MoM, -- Monthly growth
+obm.profit, obm.marge, obm.quantity,
 sum(s.target) as target,
 sum(s.target) - obm.amount as diff_amount_target
 from order_by_month obm
 inner join sales_target s on s.month_of_order_date = obm.order_date_label
-group by obm.order_date_label, obm.order_month, obm.amount, obm.profit, obm.quantity
+group by obm.order_date_label, obm.order_month, obm.amount, obm.profit, obm.marge, obm.quantity
 order by obm.order_month;
 
 -- Compare sales with target by category
@@ -236,6 +252,7 @@ select
 o.category,
 sum(o.amount) as sum_amount_category,
 sum(o.profit) as sum_profit_category,
+sum(profit)/sum(amount) as marge,
 sum(o.quantity) as sum_quantity_category,
 s.sum_target_category
 from clean_ecomm.order_details o
@@ -259,7 +276,8 @@ inner join clean_ecomm.order_details o on o.order_id = l.order_id
 group by date_trunc('month', l.order_date), to_char(l.order_date,'Mon-YY'), o.category)
 
 select
-oc.order_date, oc.category, oc.sum_amount_per_mcat, oc.sum_profit_per_mcat, oc.sum_quantity_per_mcat,
+oc.order_date, oc.category, oc.sum_amount_per_mcat, oc.sum_profit_per_mcat, oc.sum_profit_per_mcat/oc.sum_amount_per_mcat as marge,
+oc.sum_quantity_per_mcat,
 st.target
 from order_by_month_cat oc
 inner join clean_ecomm.sales_target st on st.month_of_order_date = oc.order_date and st.category = oc.category
@@ -271,8 +289,9 @@ order by oc.order_month, oc.category, oc.sum_profit_per_mcat desc;
 select
 l.customername,
 l.order_id, 
-sum(o.amount) as amount_per_customer, 
+sum(o.amount) as amount_per_customer,
 sum(o.profit) as profit_per_customer,
+sum(profit)/sum(amount) as marge,
 avg(o.profit) as avgprofit_per_customer,
 sum(o.quantity) as quantoty_per_customer
 from clean_ecomm.list_of_orders l
@@ -288,6 +307,7 @@ l.state,
 l.city,
 sum(o.amount) as amount_per_city,
 sum(o.profit) as profit_per_city,
+sum(profit)/sum(amount) as marge,
 sum(o.quantity) as quantity_per_city
 from clean_ecomm.list_of_orders l
 inner join clean_ecomm.order_details as o
@@ -297,10 +317,94 @@ group by l.state, l.city), ranked as
 cume_dist() over(order by profit_per_city desc) as pr from city_profit -- cumulative proportion of lines whose value is ≤ the current value
 )
 
-select state, city, amount_per_city, profit_per_city, quantity_per_city
+select state, city, amount_per_city, profit_per_city, marge, quantity_per_city
 from ranked
 where pr <= 0.33 -- curent value for the top cities we want to retain
 order by profit_per_city desc;
 
--- sales trends, profits, and comparison vs. target by quarter
+-------------------------------------- INTERMEDIATE TABLES OR VIEWS TO SIMPLIFY POWER BI -------------------------------------------------------
+
+--sales_summary : Revenue, profit, margin by category/subcategory and month
+
+create table clean_ecomm.sales_summary as
+with order_by_month_cat as
+(select
+to_char(l.order_date,'Mon-YY') as order_date_text, -- display format MMM-YY ex Apr-18 (month of the year)
+date_trunc('month', l.order_date)::date as order_month, --Type date with month as unit
+o.category,
+o.sub_category,
+sum(o.amount) as sum_amount_per_sub, -- we want the revenue total by month, same for the profit and quantity
+sum(o.profit) as sum_profit_per_sub,
+sum(o.quantity) as sum_quantity_per_sub
+from clean_ecomm.list_of_orders l
+inner join clean_ecomm.order_details o on o.order_id = l.order_id
+group by date_trunc('month', l.order_date), to_char(l.order_date,'Mon-YY'), o.category, o.sub_category)
+
+select
+oc.order_date_text as order_date, 
+oc.category, oc.sub_category, oc.sum_amount_per_sub, 
+COALESCE(oc.sum_profit_per_sub / NULLIF(oc.sum_amount_per_sub, 0), 0) AS marge,
+oc.sum_quantity_per_sub,
+st.target
+from order_by_month_cat oc
+inner join clean_ecomm.sales_target st on to_char(oc.order_month, 'Mon-YY') = st.month_of_order_date and st.category = oc.category
+order by oc.order_month, oc.category, oc.sub_category, oc.sum_profit_per_sub desc;
+
+-- client_summary: Total revenue, total profit, number of orders per customer and per city
+
+create table clean_ecomm.client_summary as
+select
+l.state,
+l.city,
+l.customername,
+sum(o.amount) as amount_per_customer,
+sum(o.profit) as profit_per_customer,
+100 * sum(o.profit) / NULLIF(sum(o.amount), 0) as marge,
+sum(o.quantity) as quantity_per_customer
+from clean_ecomm.list_of_orders l
+inner join clean_ecomm.order_details o on o.order_id = l.order_id 
+group by l.customername, l.state, l.city
+order by l.state, l.city, amount_per_customer;
+
+-- performance_vs_target: Actual revenue vs. target by category and month, with variance and % achieved.
+
+create table clean_ecomm.performance_vs_target as
+with order_by_month_cat as
+(select
+to_char(l.order_date,'Mon-YY') as order_date, -- display format MMM-YY ex Apr-18 (month of the year)
+date_trunc('month', l.order_date) as order_month, --Type date with month as unit
+o.category,
+sum(o.amount) as sum_amount_per_mcat -- we want the revenue total by month and category
+from clean_ecomm.list_of_orders l
+inner join clean_ecomm.order_details o on o.order_id = l.order_id
+group by date_trunc('month', l.order_date), to_char(l.order_date,'Mon-YY'), o.category)
+
+select
+oc.order_date, oc.category, oc.sum_amount_per_mcat,
+st.target,
+oc.sum_amount_per_mcat - st.target as Diff_amount_target,
+100*oc.sum_amount_per_mcat/st.target as achievement
+from order_by_month_cat oc
+inner join clean_ecomm.sales_target st on st.month_of_order_date = oc.order_date and st.category = oc.category
+order by oc.order_month, oc.category;
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
